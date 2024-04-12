fc3750a999310b92f7198d0589ec6abe42e38639
Shivam Bhardwaj <Shivambhardwaj8585@gmail.com>
2024-04-12 19:29:41 +0530

    Added Hero section frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Camp, Features, GetApp, Guide, Hero } from "@/components";
 
 
 export default function Home() {
   return (
     <>
-      
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
     </>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,19 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Button from "./Button";
+import Hero from "./Hero";
+import Guide from "./Guide";
+import Features from "./Features";
+import Camp from "./Camp";
+import GetApp from "./GetApp";
 
 export {
     Navbar,
     Footer,
     Button,
-    
+    Hero,
+    Guide,
+    Features,
+    Camp,
+    GetApp,
 }
